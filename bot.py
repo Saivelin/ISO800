4740445eea144af42a0addd433323b75746f16da
Saivelin <vlad.pochkalov@mail.ru>
2022-12-02 21:02:34 +0300

    first commit

--- bot.py
+++ bot.py
@@ -2,25 +2,21 @@ import telebot
 from telebot import types
 import TOKEN
 from xml.etree.ElementTree import tostring
-# from requests import patch, request
 import sqlite3
 from pathlib import Path
 bot = telebot.TeleBot(TOKEN.token)  # токен лежит в файле config.py
-
-
-# @bot.message_handler(commands=['start'])  # создаем команду
-# def start(message):
-# markup = types.InlineKeyboardMarkup()
-# button1 = types.InlineKeyboardButton(
-#     "Сайт Хабр", url='https://habr.com/ru/all/')
-# markup.add(button1)
-# bot.send_message(message.chat.id, "Привет, {0.first_name}! Нажми на кнопку и перейди на сайт)".format(
-#     message.from_user), reply_markup=markup)
 startMessage = 'Вас приветствует виртуальный помощник креативного пространства ИСО800. Здесь вы можете забронировать площадку для фото и видеосъемки, арендовать технику, записаться на мероприятия и многое другое'
 mainChatId = "@iso800nn"  # -1001595345813
 subscToCh = "Чтобы начать пользоваться ботом, необходимо быть подписанным на канал ИСО800"
 notSubText = "Не подписан"
 subText = 'Подписан'
+btn1txt = 'ОБ ИСО'
+btn2txt = 'АРЕНДА ПРОСТРАНСТВА И ОБОРУДОВАНИЯ'
+btn3txt = 'КИНОВЕЧЕРА И РАЗВЛЕЧЕНИЯ'
+btn4txt = 'КУРСЫ И МАСТЕР-КЛАССЫ'
+btn5txt = 'УСЛУГИ'
+btn2txttxt1 = "Аренда пространства"
+btn2txttxtsec = "Аренда оборудования"
 
 
 def check_sub_channel(chat_m):
@@ -33,20 +29,33 @@ def check_sub_channel(chat_m):
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # btn1 = types.KeyboardButton("👋 Поздороваться")
-    # btn2 = types.KeyboardButton("❓ Задать вопрос")
-    # markup.add(btn1, btn2)
-    print(message.from_user)
-    uid = message.from_user.id
-    print(uid)
-    print(bot.get_chat_member(chat_id=mainChatId, user_id=uid))
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton(btn1txt)
+    btn2 = types.KeyboardButton(btn2txt)
+    btn3 = types.KeyboardButton(btn3txt)
+    btn4 = types.KeyboardButton(btn4txt)
+    btn5 = types.KeyboardButton(btn5txt)
+    markup.add(btn1, btn2, btn3, btn4, btn5)
     bot.send_message(message.chat.id, text=startMessage.format(
-        message.from_user))
-    # if check_sub_channel(bot.get_chat_member(chat_id=mainChatId, user_id=uid)):
-    #     bot.send_message(message.chat_id, text=subText)
-    # else:
-    #     bot.send_message(message.chat_id, text=notSubText)
+        message.from_user), reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == btn1txt):
+        bot.send_message(
+            message.chat.id, text="краткая информация о пространстве с фотографиями или видео")
+    elif(message.text == btn2txt):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton(btn2txttxt1)
+        btn2 = types.KeyboardButton(btn2txttxtsec)
+        markup.add(btn1, btn2)
+        bot.send_message(
+            message.chat.id, text="краткая информация о пространстве с фотографиями или видео", reply_markup=markup)
+    elif(message.text == btn2txttxt1):
+        bot.send_message(message.chat.id, text=btn2txttxt1)
+    elif(message.text == btn2txttxtsec):
+        bot.send_message(message.chat.id, text=btn2txttxtsec)
 
 
 bot.polling(none_stop=True)
