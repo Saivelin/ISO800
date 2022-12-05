b3aadd8eb27a249a92ac5ab70ad6172a584e13bf
Saivelin <vlad.pochkalov@mail.ru>
2022-12-05 15:05:53 +0300

    first commit

--- bot.py
+++ bot.py
@@ -4,7 +4,9 @@ import TOKEN
 from xml.etree.ElementTree import tostring
 import sqlite3
 from pathlib import Path
+
 bot = telebot.TeleBot(TOKEN.token)  # токен лежит в файле config.py
+
 startMessage = 'Вас приветствует виртуальный помощник креативного пространства ИСО800. Здесь вы можете забронировать площадку для фото и видеосъемки, арендовать технику, записаться на мероприятия и многое другое'
 mainChatId = "@iso800nn"  # -1001595345813
 subscToCh = "Чтобы начать пользоваться ботом, необходимо быть подписанным на канал ИСО800"
@@ -18,6 +20,9 @@ btn5txt = 'УСЛУГИ'
 btn2txttxt1 = "Аренда пространства"
 btn2txttxtsec = "Аренда оборудования"
 
+db = sqlite3.connect('bd.sqlite')
+sql = db.cursor()
+
 
 def check_sub_channel(chat_m):
     print(chat_m['status'])
@@ -27,17 +32,23 @@ def check_sub_channel(chat_m):
         return False
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
+def mainMenuBack():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton(btn1txt)
     btn2 = types.KeyboardButton(btn2txt)
     btn3 = types.KeyboardButton(btn3txt)
     btn4 = types.KeyboardButton(btn4txt)
     btn5 = types.KeyboardButton(btn5txt)
-    markup.add(btn1, btn2, btn3, btn4, btn5)
+    markup.row(btn1)
+    markup.row(btn2, btn3)
+    markup.row(btn4, btn5)
+    return markup
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
     bot.send_message(message.chat.id, text=startMessage.format(
-        message.from_user), reply_markup=markup)
+        message.from_user), reply_markup=mainMenuBack())
 
 
 @bot.message_handler(content_types=['text'])
